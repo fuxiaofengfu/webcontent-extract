926cdaa589b331925defd71fd303f93486150085
xiaofengfu <fuxiaofeng1107@163.com>
2018-01-20 15:08:52 +0800

    update

--- shell/run_extract.sh
+++ shell/run_extract.sh
@@ -5,17 +5,17 @@ contentout_path="/user/fuxiaofeng/webcontent/contentout"
 log_path="/home/fuxiaofeng/extract.log"
 hadoop_path=`which hadoop`
 jar_path="/home/fuxiaofeng/webcontent-extract-1.0-SNAPSHOT.jar"
-
+add_libs="/home/fuxiaofeng/libs/JsoupXpath-0.3.2.jar,/home/fuxiaofeng/libs/jsoup-1.9.2.jar,/home/fuxiaofeng/libs/htmlcleaner-2.9.jar"
 echo "$hadoop_path"
 
 # 提取网页正文的xpath
 nohup $hadoop_path jar $jar_path extractxpath -Dinput.path=$input_path \
--Doutput.path=$xpathout_path >> $log_path 2>&1 &
+-Doutput.path=$xpathout_path -libjars $add_libs >> $log_path 2>&1 &
 
 # 统计满足可用的content_xpath
 nohup $hadoop_path jar $jar_path countxpath >> $log_path 2>&1 &
 
 # 根据可用的xpath提取爬取网页的内容
 nohup $hadoop_path jar $jar_path extractcontent -Dinput.path=$input_path \
--Doutput.path=$contentout_path >> $log_path 2>&1 &
+-Doutput.path=$contentout_path -libjars $add_libs >> $log_path 2>&1 &
 
